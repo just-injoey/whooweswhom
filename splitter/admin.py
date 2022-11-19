2bdf8e49e34d25fdd51be5cd03603ce9d11e660c
Justin T Joseph <jtjjust@gmail.com>
2022-11-19 10:13:59 +0530

    Update models.py & admin.py

--- splitter/admin.py
+++ splitter/admin.py
@@ -2,8 +2,14 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib.auth.admin import UserAdmin
-from .models import User
-# ,room,room_members,transaction,debt,final_transactions,Personal_income,Personal_expense
+from .models import User,room,room_members,transaction,debt,final_transactions,Personal_income,Personal_expense
 # Register your models here.
 
 admin.site.register(User, UserAdmin)
+admin.site.register(room)
+admin.site.register(room_members)
+admin.site.register(transaction)
+admin.site.register(debt)
+admin.site.register(final_transactions)
+admin.site.register(Personal_income)
+admin.site.register(Personal_expense)
