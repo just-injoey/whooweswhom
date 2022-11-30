16cacf4ef18a04f170573afc6d4cc1446605aaad
Justin T Joseph <93005964+just-injoey@users.noreply.github.com>
2022-11-30 12:06:12 +0530

    Update settings.py

--- wow/settings.py
+++ wow/settings.py
@@ -157,7 +157,7 @@ SOCIALACCOUNT_PROVIDERS = {
 "google": {
     "APP": {
         "client_id": "788851246302-isb6ahrlrvhisvrq3bmuekjq9jr74f0h.apps.googleusercontent.com",
-        "secret": "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",
+        "secret": "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",
     },
 },
 }
