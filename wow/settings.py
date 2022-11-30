c05f8e104b8e59267b7bac90b0bf03599f493cab
Justin T Joseph <93005964+just-injoey@users.noreply.github.com>
2022-11-30 10:31:16 +0530

    Update settings.py

--- wow/settings.py
+++ wow/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-4gm$^^_pw*sd)gsachu96nt26l)kcaz5i3&2141xi#1oxjk760
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+# ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'web-production-60e5.up.railway.app']
 
 # Application definition
 
