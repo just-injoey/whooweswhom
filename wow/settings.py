c77cb18b8517e78bfef079df3710def60ddc5c20
Justin T Joseph <93005964+just-injoey@users.noreply.github.com>
2022-12-07 09:40:41 +0530

    Update settings.py

--- wow/settings.py
+++ wow/settings.py
@@ -28,8 +28,10 @@ DEBUG = True
 
 CSRF_TRUSTED_ORIGINS = ['https://web-production-60e5.up.railway.app']
 # CSRF_TRUSTED_ORIGINS = ['https://web-production-1afb.up.railway.app']
-ALLOWED_HOSTS = ['web-production-1afb.up.railway.app']
+# ALLOWED_HOSTS = ['web-production-1afb.up.railway.app']
 # ALLOWED_HOSTS = ['web-production-60e5.up.railway.app']
+ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
